489e25bd0e29c6d514ac5edd001b502b65154b95
MagB93 <magnus.leo93@gmail.com>
2018-06-01 20:45:51 +0200

    Added basic bspline functions

--- modules/basis/bspline_m.f90
+++ modules/basis/bspline_m.f90
@@ -1,7 +1,61 @@
-module BSpline
+module BSpline_m
 
-  type knotVector
+  use Settings_m 
+  use Conversion_m
 
-  end type knotVector
+  implicit none
 
-end module BSpline
+  type BSplineBasis_t
+    real ( kind = real_w ), allocatable :: knots_( : )
+    integer ( kind = int_w ) :: index_
+    integer ( kind = int_w ) :: order_
+
+    contains
+      procedure :: basisFunc => N
+  end type BSplineBasis_t
+
+contains
+  pure function N( this, u ) result ( N_i_p )
+    class( BSplineBasis_t ), intent( in ) :: this
+    real( kind = real_w ), intent( in ) :: u
+    real( kind = real_w ) :: N_i_p
+
+    N_i_p = computeBasis( this%knots_, this%index_, this%order_, u )
+
+  end function N
+
+  pure recursive function computeBasis( knots, index, order, u ) result( N_i_p )
+    real ( kind = real_w ), intent( in ) :: knots( : )
+    integer ( kind = int_w ), intent( in ) :: index
+    integer ( kind = int_w ), intent( in ) :: order
+    real ( kind = real_w ), intent( in ) :: u
+    real ( kind = real_w ) :: N_i_p
+    real ( kind = real_w ) :: left
+    real ( kind = real_w ) :: right
+
+    if ( order .EQ. 0 ) then
+      N_i_p = logic2dbl( knots( index ) <= u .AND. u < knots( index + 1 ) )
+    else
+      if ( u < knots( index ) .OR. u > knots( index + 1 ) ) then
+        N_i_p = 0.
+      else
+        left = ( u - knots( index ) ) / ( knots( index + order ) - knots( index ) )
+        right = ( knots( index + order + 1 ) - u ) / ( knots( index + order + 1 ) - knots( index + 1 )  )
+
+        if( isnan(  left ) ) then
+         left = 0.
+        endif
+        if( isnan( right ) ) then
+          right = 0.
+        endif
+
+        N_i_p = left * computeBasis( knots, index, order - 1, u ) + &
+                right * computeBasis( knots, index + 1, order - 1, u )
+      endif
+
+    endif
+
+  end function computeBasis
+
+
+end module BSpline_m
