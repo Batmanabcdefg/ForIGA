fb85aba072f254246889c2d54ab2820dfb48dd70
MagB93 <magnus.leo93@gmail.com>
2018-06-02 11:41:21 +0200

    Shortened the precision kind settings

--- modules/basis/bspline_m.f90
+++ modules/basis/bspline_m.f90
@@ -6,9 +6,9 @@ module BSpline_m
   implicit none
 
   type BSplineBasis_t
-    real ( kind = real_w ), allocatable :: knots_( : )
-    integer ( kind = int_w ) :: index_
-    integer ( kind = int_w ) :: order_
+    real ( kind = rw ), allocatable :: knots_( : )
+    integer ( kind = iw ) :: index_
+    integer ( kind = iw ) :: order_
 
     contains
       procedure :: basisFunc => N
@@ -17,21 +17,21 @@ module BSpline_m
 contains
   pure function N( this, u ) result ( N_i_p )
     class( BSplineBasis_t ), intent( in ) :: this
-    real( kind = real_w ), intent( in ) :: u
-    real( kind = real_w ) :: N_i_p
+    real( kind = rw ), intent( in ) :: u
+    real( kind = rw ) :: N_i_p
 
     N_i_p = computeBasis( this%knots_, this%index_, this%order_, u )
 
   end function N
 
   pure recursive function computeBasis( knots, index, order, u ) result( N_i_p )
-    real ( kind = real_w ), intent( in ) :: knots( : )
-    integer ( kind = int_w ), intent( in ) :: index
-    integer ( kind = int_w ), intent( in ) :: order
-    real ( kind = real_w ), intent( in ) :: u
-    real ( kind = real_w ) :: N_i_p
-    real ( kind = real_w ) :: left
-    real ( kind = real_w ) :: right
+    real ( kind = rw ), intent( in ) :: knots( : )
+    integer ( kind = iw ), intent( in ) :: index
+    integer ( kind = iw ), intent( in ) :: order
+    real ( kind = rw ), intent( in ) :: u
+    real ( kind = rw ) :: N_i_p
+    real ( kind = rw ) :: left
+    real ( kind = rw ) :: right
 
     if ( order .EQ. 0 ) then
       N_i_p = logic2dbl( knots( index ) <= u .AND. u < knots( index + 1 ) )
--- modules/util/settings_m.f90
+++ modules/util/settings_m.f90
@@ -4,8 +4,8 @@ module Settings_m
 
   use iso_fortran_env
   
-  integer, parameter :: real_w = 8 ! by default every real data is in double precision
-  integer, parameter :: int_w = 8 ! by default every interger data has 8 bytes
-  integer, parameter :: imp_w = 2 ! Number of images
+  integer, parameter :: rw = 8 ! by default every real data is in double precision
+  integer, parameter :: iw = 8 ! by default every interger data has 8 bytes
+  integer, parameter :: images = 2 ! Number of images
 
 end module Settings_m
